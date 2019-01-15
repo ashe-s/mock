bd76c4673affc56341b43a6d894f4311b10a4e18
ashe-s <seshimo-asako@kayac.com>
2019-01-15 17:23:45 +0900

    add sample code

--- src/js/lib/Concentration.js
+++ src/js/lib/Concentration.js
@@ -4,18 +4,14 @@ import shuffle from '@/util/shuffleArray'
 /***
  * 設定
  */
-// canvas
-const app = new PIXI.Application({});
-
 // 基本設定
 const settings = {
     maxSuccess: 1, // n回正解したらゲームクリア
     maxFailure: 3, // n回連続で失敗したらゲームオーバー？
     cardW: 200,
     cardH: 300,
-    margin: 20
+    marginLow: 20
 }
-
 
 // 元となるカードのデータ
 const lawCards = [{
@@ -127,6 +123,9 @@ function gameOver() {
 /**
  * カードを表示
  */
+// canvas
+const app = new PIXI.Application({});
+
 function drawCards() {
     // canvasを使うはず
     // isOpened = false のやつは等しく裏を表示
@@ -140,3 +139,108 @@ function drawCards() {
 initialise()
 
 console.log(fieldCards)
+
+// var app = new PIXI.Application();
+// document.body.appendChild(app.view);
+
+// create a background...
+// var background = PIXI.Sprite.fromImage('required/assets/button_test_BG.jpg');
+// background.width = app.screen.width;
+// background.height = app.screen.height;
+
+// add background to stage...
+// app.stage.addChild(background);
+
+// create some textures from an image path
+var textureButton = PIXI.Texture.fromImage('required/assets/button.png');
+var textureButtonDown = PIXI.Texture.fromImage('required/assets/buttonDown.png');
+var textureButtonOver = PIXI.Texture.fromImage('required/assets/buttonOver.png');
+
+var buttons = [];
+
+var buttonPositions = [
+    175, 75,
+    655, 75,
+    410, 325,
+    150, 465,
+    685, 445
+];
+
+for (var i = 0; i < 5; i++) {
+    var button = new PIXI.Sprite(textureButton);
+    button.buttonMode = true;
+
+    button.anchor.set(0.5);
+    button.x = buttonPositions[i * 2];
+    button.y = buttonPositions[i * 2 + 1];
+
+    // make the button interactive...
+    button.interactive = true;
+    button.buttonMode = true;
+
+    button
+        // Mouse & touch events are normalized into
+        // the pointer* events for handling different
+        // button events.
+        .on('pointerdown', onButtonDown)
+        .on('pointerup', onButtonUp)
+        .on('pointerupoutside', onButtonUp)
+        .on('pointerover', onButtonOver)
+        .on('pointerout', onButtonOut);
+
+    // Use mouse-only events
+    // .on('mousedown', onButtonDown)
+    // .on('mouseup', onButtonUp)
+    // .on('mouseupoutside', onButtonUp)
+    // .on('mouseover', onButtonOver)
+    // .on('mouseout', onButtonOut)
+
+    // Use touch-only events
+    // .on('touchstart', onButtonDown)
+    // .on('touchend', onButtonUp)
+    // .on('touchendoutside', onButtonUp)
+
+    // add it to the stage
+    app.stage.addChild(button);
+
+    // add button to array
+    buttons.push(button);
+}
+
+// set some silly values...
+buttons[0].scale.set(1.2);
+buttons[2].rotation = Math.PI / 10;
+buttons[3].scale.set(0.8);
+buttons[4].scale.set(0.8, 1.2);
+buttons[4].rotation = Math.PI;
+
+function onButtonDown() {
+    this.isdown = true;
+    this.texture = textureButtonDown;
+    this.alpha = 1;
+}
+
+function onButtonUp() {
+    this.isdown = false;
+    if (this.isOver) {
+        this.texture = textureButtonOver;
+    } else {
+        this.texture = textureButton;
+    }
+}
+
+function onButtonOver() {
+    this.isOver = true;
+    if (this.isdown) {
+        return;
+    }
+    this.texture = textureButtonOver;
+}
+
+function onButtonOut() {
+    this.isOver = false;
+    if (this.isdown) {
+        return;
+    }
+    this.texture = textureButton;
+}
